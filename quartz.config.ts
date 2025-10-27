54bea901618fc5b8c1881605088e151392f65e57
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-10-27 12:29:24 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Insurance Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
