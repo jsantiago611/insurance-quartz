2eec6c49d8d686fe2ad081a5e786f863397d7cd4
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-11-03 20:17:30 +0000

    font file titles

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "National2Condensed",
-        body: "GT-National",
+        body: "GT-America",
         code: "IBM Plex Mono",
       },
       colors: {
