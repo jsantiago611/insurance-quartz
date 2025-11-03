29cd3c5ea08ef307eab10c8db7b67d5ba31fa984
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-11-03 20:38:09 +0000

    removes -

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "National2Condensed",
-        body: "GT-America",
+        body: "GTAmerica",
         code: "IBM Plex Mono",
       },
       colors: {
