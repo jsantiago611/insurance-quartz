2eb6347b4b3657498824a56e8527879e50a65929
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-11-03 20:56:32 +0000

    body back to source

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "National2Condensed",
-        body: "GTAmerica",
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
