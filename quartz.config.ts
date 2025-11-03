33ebe6abb61653a1f7af960c1f9f1f62aab0bb6f
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-11-03 20:32:41 +0000

    REVERTING

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "National2Condensed",
-        body: "GT-America",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
